9db9411227c18a48d7e1195e66342fa6f5f7b013
vishalicious213 <vishalicious213@gmail.com>
2026-01-22 21:31:11 -0800

    initial commit

---
+++ 2026/01-jan/2026-01-03.js
@@ -0,0 +1,20 @@
+// https://www.freecodecamp.org/learn/daily-coding-challenge/2026-01-03
+
+function findLeftHandedSeats(table) {
+  const top = table[0]
+  const bottom = table[1]
+  let seats = 0
+
+  top.forEach((seat, index) => {
+    if (seat === "U" && top[index + 1] != "R") seats++
+  })
+  console.log("top", top, seats)
+
+  bottom.forEach((seat, index) => {
+    if (seat === "U" && bottom[index - 1] != "R") seats++
+  })
+  console.log("bot", bottom, seats)
+
+  console.log(seats)
+  return seats;
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# freeCodeCamp Daily Coding Challenges
+
+https://www.freecodecamp.org/learn/daily-coding-challenge/archive
