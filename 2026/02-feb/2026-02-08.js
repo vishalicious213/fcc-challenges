7d6e3ef241cccd311886769d12b0205a6be557ec
vishalicious213 <vishalicious213@gmail.com>
2026-02-10 12:28:37 -0800

    updated /2026-02-08 with single-pass .reduce examples

--- 2026/02-feb/2026-02-08.js
+++ 2026/02-feb/2026-02-08.js
@@ -7,3 +7,20 @@ function calculatePenaltyDistance(rounds) {
     .map(round => (5 - parseInt(round)) * 150)
     .reduce((total, curr) => total + curr, 0)
 }
+
+// single-pass version using .reduce()
+
+function calculatePenaltyDistance(rounds) {
+  return rounds.reduce((total, round) => {
+    return total + (5 - parseInt(round)) * 150
+  }, 0)
+}
+
+
+// tighter .reduce() version
+
+const calculatePenaltyDistance = rounds =>
+  rounds.reduce(
+    (total, round) => total + (5 - parseInt(round)) * 150,
+    0
+  )
