53b18cd54cc45c3495b39a583255700c76fde2a4
vishalicious213 <vishalicious213@gmail.com>
2026-02-10 12:41:39 -0800

    updated /2026-02-08

--- 2026/02-feb/2026-02-08.js
+++ 2026/02-feb/2026-02-08.js
@@ -1,10 +1,15 @@
 // #182
 // https://www.freecodecamp.org/learn/daily-coding-challenge/2026-02-08
 
+// Initially, we used parseInt() for these examples, but changed to Number() because:
+// parseInt() trims whitespace, walks the string char by char, stops parsing at first invalid char & returns what it has so far
+// Number() parses entire string, anything invalid = NaN, no partial parsing, less work
+// parseInt() can hide bad input, Number() fails loudly & returns a number or NaN
+
 
 function calculatePenaltyDistance(rounds) {
   return rounds
-    .map(round => (5 - parseInt(round)) * 150)
+    .map(round => (5 - Number(round)) * 150)
     .reduce((total, curr) => total + curr, 0)
 }
 
@@ -13,7 +18,7 @@ function calculatePenaltyDistance(rounds) {
 
 function calculatePenaltyDistance(rounds) {
   return rounds.reduce((total, round) => {
-    return total + (5 - parseInt(round)) * 150
+    return total + (5 - Number(round)) * 150
   }, 0)
 }
 
@@ -22,7 +27,7 @@ function calculatePenaltyDistance(rounds) {
 
 const calculatePenaltyDistance = rounds =>
   rounds.reduce(
-    (total, round) => total + (5 - parseInt(round)) * 150,
+    (total, round) => total + (5 - Number(round)) * 150,
     0
   )
 
@@ -33,7 +38,7 @@ function calculatePenaltyDistance(rounds) {
   let total = 0
 
   rounds.forEach(round => {
-    total += (5 - parseInt(round)) * 150
+    total += (5 - Number(round)) * 150
   })
 
   return total
