9cce804775e25afe56a8ae3fccda41fd371b18a6
vishalicious213 <vishalicious213@gmail.com>
2026-02-10 13:03:22 -0800

    updated /2026-02-09

--- 2026/02-feb/2026-02-09.js
+++ 2026/02-feb/2026-02-09.js
@@ -2,14 +2,16 @@
 // https://www.freecodecamp.org/learn/daily-coding-challenge/2026-02-09
 
 function skiJumpMedal(distancePoints, stylePoints, windComp, kPointBonus) {
-  const scores = [165.5, 172.0, 158.0, 180.0, 169.5, 175.0, 162.0, 170.0].sort((a, b) => b - a)
-  console.log(scores)
+  const competitorScores = [165.5, 172.0, 158.0, 180.0, 169.5, 175.0, 162.0, 170.0]
+  .sort((a, b) => b - a)
+//   console.log(competitorScores)
 
   const yourScore = distancePoints + stylePoints + windComp + kPointBonus
-  console.log(yourScore)
+//   console.log(yourScore)
   
-  if (yourScore >= scores[0]) return "Gold"
-  if (yourScore >= scores[1]) return "Silver"
-  if (yourScore >= scores[2]) return "Bronze"
+  if (yourScore >= competitorScores[0]) return "Gold"
+  if (yourScore >= competitorScores[1]) return "Silver"
+  if (yourScore >= competitorScores[2]) return "Bronze"
+  
   return "No Medal"
 }
